7b354395bd8c12fb5381cb95a3ae1cbe43b56d47
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-04-22 21:51:16 +0200

    net5.0 update

--- src/Trill.Pusher/Extensions.cs
+++ src/Trill.Pusher/Extensions.cs
@@ -2,16 +2,28 @@ using System.Linq;
 using System.Text;
 using Convey.MessageBrokers;
 using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace Trill.Pusher
 {
     internal static class Extensions
     {
         internal static CorrelationContext GetCorrelationContext(this IHttpContextAccessor accessor)
-            => accessor.HttpContext?.Request.Headers.TryGetValue("Correlation-Context", out var json) is true
-                ? JsonConvert.DeserializeObject<CorrelationContext>(json.FirstOrDefault())
-                : null;
+        {
+            if (accessor.HttpContext is null)
+            {
+                return null;
+            }
+
+            if (!accessor.HttpContext.Request.Headers.TryGetValue("Correlation-Context", out var json))
+            {
+                return null;
+            }
+
+            var value = json.FirstOrDefault();
+
+            return string.IsNullOrWhiteSpace(value) ? null : JsonSerializer.Deserialize<CorrelationContext>(value);
+        }
         
         internal static string GetSpanContext(this IMessageProperties messageProperties, string header)
         {
